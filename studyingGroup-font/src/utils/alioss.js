fa67a4a37493f59b1a80aa7402e2aa0e670954f1
yueryaa <141549879+yueryaa@users.noreply.github.com>
2025-09-21 23:53:07 +0800

    Update alioss.js

--- studyingGroup-font/src/utils/alioss.js
+++ studyingGroup-font/src/utils/alioss.js
@@ -2,9 +2,10 @@ import OSS from 'ali-oss';
 export function client() {
   var client = new OSS({
     region: 'oss-cn-beijing',
-    accessKeyId: 'LTAI5tC38HLYH37oTfU5Z8oT',
-    accessKeySecret: 'BSTLBiLuvG7z7j5FLS8qCrHsKoUxN0',
+    accessKeyId: '***',
+    accessKeySecret: '***',
     bucket: 'userp'
   })
   return client
+
 }
